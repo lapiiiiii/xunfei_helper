e34500e54b02f9e33be2692808ee8244d9af2158
lapi <1015249773@qq.com>
2024-05-01 11:29:18 +0800

    已完成聊天页面的设计

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ app.$mount()
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
+
 export function createApp() {
   const app = createSSRApp(App)
   return {
     app
   }
 }
+
 // #endif
--- pages/recite/recite.vue
+++ pages/recite/recite.vue
@@ -1,179 +1,241 @@
-
-
 <template>
-  <view class="container">
-    <view class="nav-bar">
-      <!-- 左侧返回按钮 -->
-      <view class="back-btn" >
-          <image  class="back-image" mode="widthFix"></image>
+  <view class="chat">
+    <scroll-view :style="{height: `${windowHeight}rpx`}"
+                 id="scrollview"
+                 scroll-y="true"
+                 :scroll-top="scrollTop"
+                 :scroll-with-animation="true"
+                 class="scroll-view">
+      <!-- 聊天主体 -->
+      <view id="msglistview" class="chat-body">
+        <!-- 聊天记录 -->
+        <view v-for="(item,index) in list" :key="index"  >
+          <!-- 自己发的消息 -->
+          <view class="item self" v-if="item.me!=''">
+            <!-- 文字内容 -->
+            <view class="content right">{{ item.me }}</view>
+          </view>
+          <!-- 机器人发的消息 -->
+          <view class="item Ai" v-if="item.robot!=''" >
+            <!-- 文字内容 -->
+            <view class="content left">{{ item.robot }}</view>
+          </view>
+        </view>
       </view>
-      <!-- 中间标题 -->
-      <view class="title">{{ pageTitle }}</view>
+    </scroll-view>
+    <!-- 底部消息发送栏 -->
+    <!-- 用来占位，防止聊天消息被发送框遮挡 -->
+    <view class="chat-bottom">
+      <view class="send-msg">
+        <view class="uni-textarea">
+          <textarea v-model="inputValue"
+                    maxlength="300"
+                    :show-confirm-bar="false"
+                    auto-height
+                    placeholder="请输入古诗文的名字"></textarea>
+        </view>
+        <button @click="handleEnter" class="send-btn">发送</button>
+      </view>
     </view>
-
-
-   <view class="custom-container">
-     <div class="container-text">
-       <p>哈喽~<image class="hello"  mode="widthFix"></image></p>
-       <p>想要检查你的背诵成果吗？</p>
-       <p>先输入你要背诵的古诗文的名字吧~</p>
-       <p>正确的地方会用<span class="wrong">红色</span>标出来哦~</p>
-     </div>
-   </view>
-
-    <view class="content">
-       <p>{{result}}</p>
-    </view>
-
-
-    <view class="bottom-bar">
-      <input class="input-box" type="text" placeholder="请输入古诗文名字" v-model="inputValue" @confirm="handleEnter" />
-      <button class="enter-button" @tap="handleEnter"></button>
-    </view>
-	</view>
-
+  </view>
 </template>
 
+
 <script>
+import { baseUrl } from '@/config.js';
 
 export default {
   data() {
     return {
-      inputValue: '', // 添加inputValue属性
-      result: '' // 添加result属性
+      scrollTop: 0,
+      list: [
+        {
+          me: "",
+          id: 0,
+          robot: ""
+        }
+      ],
+      inputValue: ""
     };
   },
-  props: {
-    // 页面标题
-    pageTitle: {
-      type: String,
-      default: '背诵助手'
+  computed: {
+    windowHeight() {
+      return this.rpxTopx(uni.getSystemInfoSync().windowHeight);
     }
   },
- methods: {
-   handleEnter() {
-     // Check if inputValue is not empty
-     if (!this.inputValue.trim()) {
-       // Handle empty input case here if needed
-       return;
-     }
- 
-     uni.request({
-       url: 'http://localhost:8080/poem', // Concatenate baseURL with endpoint URL
-       method: 'GET',
-       data: {
-         title: this.inputValue.trim() // Trim input value to remove leading/trailing spaces
-       },
-       success: (res) => {
-         console.log('请求成功：', res.data);
-         this.result = res.data; // Assign result to the component's result property
-       },
-       fail: (err) => {
-         console.log('请求失败：', err);
-         // Handle failed request here if needed
-       }
-     });
-   }
- }
-
+  methods: {
+    rpxTopx(px) {
+      let deviceWidth = wx.getSystemInfoSync().windowWidth;
+      let rpx = (750 / deviceWidth) * Number(px);
+      return Math.floor(rpx);
+    },
+    handleEnter() {
+      const inputValue = this.inputValue;
+      if (!inputValue) {
+        uni.showToast({
+          title: "请输入古诗文名字",
+          icon: "none"
+        });
+        return;
+      }
+      this.queryPoetry(inputValue)
+        .then(robot => {
+          this.list.push({ me: inputValue, robot: robot });
+        })
+        .catch(err => {
+          console.log("请求失败：", err);
+        });
+      this.inputValue = ""; // 清空输入框
+    },
+    queryPoetry(poetryName) {
+      return new Promise((resolve, reject) => {
+        uni.request({
+          url: baseUrl + ":8088/poem",
+          method: "GET",
+          data: {
+            title: poetryName.trim()
+          },
+          success: res => {
+            console.log("请求成功：", res.data);
+            resolve(res.data);
+          },
+          fail: err => {
+            console.log("请求失败：", err);
+            reject(err);
+          }
+        });
+      });
+    }
+  }
 };
 </script>
 
-<style scoped>
-.hello{
-width:20px;
-} 
-  
-.custom-container {
-margin-top: 20px;
-padding: 10px;
-height:90px;
-border: 1px solid #007bff; /* 蓝色边框 */
-border-radius: 10px; /* 圆角 */
-box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1), -5px -5px 10px rgba(0, 0, 0, 0.1); /* 阴影效果 */
-background: linear-gradient(to bottom, #007bff, #ffffff); /* 渐变背景色 */
-}
 
-.container-text {
-color: #000000; /* 文字颜色 */
-font-size: 16px; /* 文字大小 */
-text-align:left; 
-font-weight: bold;
-} 
-.wrong{
-color:red;
-font-size: 20px;
-}
-  
-.back-image{
- width: 20px;
-height:20px;
- margin-left: 5px;
- padding-top: 18px;
- padding-right: 50px;
-}
-  
-.container {
-display: flex;
- flex-direction: column;
- height: 130vh; /* 使用视窗的高度 */
- padding: 20px;
- margin-top: 40px;
-}
 
-.nav-bar {
- width: 100%;
- height: 54px;
- background-color: #ffffff;
- display: flex;
- align-items: center;
- padding: 0 15px;
- position:fixed;
- left: 0;
- right: 0;
- top:0;
-}
 
-.title {
- font-size: 18px;
- font-weight: bold;
- color: #333333;
- margin-left: 28px;
- margin-top: 15px;
-}
+<style lang="scss" scoped>
+$chatContentbgc: #C2DCFF;
+$sendBtnbgc: #4F7DF5;
 
-.bottom-bar {
-position: fixed;
- bottom: 0;
- left: 0;
- width: 100%;
- display: flex;
-background-color: #ededed;
+.chat {
+  .scroll-view {
+    ::-webkit-scrollbar {
+      display: none;
+      width: 0 !important;
+      height: 0 !important;
+      -webkit-appearance: none;
+      background: transparent;
+      color: transparent;
+    }
+    background-color: #F6F6F6;
+
+    .chat-body {
+      display: flex;
+      flex-direction: column;
+      padding-top: 23rpx;
+
+      .self {
+        justify-content: flex-end;
+      }
+      .item {
+        display: flex;
+        padding: 23rpx 30rpx;
+
+        .right {
+          background-color: $chatContentbgc;
+        }
+        .left {
+          background-color: #FFFFFF;
+        }
+        .right::after {
+          position: absolute;
+          display: inline-block;
+          content: '';
+          width: 0;
+          height: 0;
+          left: 100%;
+          top: 10px;
+          border: 12rpx solid transparent;
+          border-left: 12rpx solid $chatContentbgc;
+        }
+        .left::after {
+          position: absolute;
+          display: inline-block;
+          content: '';
+          width: 0;
+          height: 0;
+          top: 10px;
+          right: 100%;
+          border: 12rpx solid transparent;
+          border-right: 12rpx solid #FFFFFF;
+        }
+        .content {
+          position: relative;
+          max-width: 486rpx;
+          border-radius: 8rpx;
+          word-wrap: break-word;
+          padding: 24rpx 24rpx;
+          margin: 0 24rpx;
+          border-radius: 5px;
+          font-size: 32rpx;
+          font-family: PingFang SC;
+          font-weight: 500;
+          color: #333333;
+          line-height: 42rpx;
+        }
+      }
+    }
   }
 
-.input-box {
- flex: 1;
- height: 30px;
- width: 300px;
- border: 1px solid #ccc;
- border-radius: 4px;
- font-size: 14px;
- background-color: #fff;
- margin: 10px; /* 调整输入框距离底部边框的距离 */
- margin-left: 15px;
-}
+  .chat-bottom {
+    width: 100%;
+    height: 177rpx;
+    background: #F4F5F7;
 
-.enter-button {
- width: 80px;
- height: 36px;
- background-color: transparent; /* Enter按钮的背景色 */
- border: none;
- border-radius: 50%;
+    .send-msg {
+      display: flex;
+      align-items: flex-end;
+      padding: 16rpx 30rpx;
+      width: 100%;
+      min-height: 177rpx;
+      position: fixed;
+      bottom: 0;
+      background: #EDEDED;
+    }
 
-}
-.enter-icon{
- width:25px;
- height: 25px;
- margin-top: 10px;
+    .uni-textarea {
+      padding-bottom: 70rpx;
+
+      textarea {
+        width: 537rpx;
+        min-height: 75rpx;
+        max-height: 500rpx;
+        background: #FFFFFF;
+        border-radius: 8rpx;
+        font-size: 32rpx;
+        font-family: PingFang SC;
+        color: #333333;
+        line-height: 43rpx;
+        padding: 5rpx 8rpx;
+      }
+    }
+
+    .send-btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 70rpx;
+      margin-left: 25rpx;
+      width: 128rpx;
+      height: 75rpx;
+      background: $sendBtnbgc;
+      border-radius: 8rpx;
+      font-size: 28rpx;
+      font-family: PingFang SC;
+      font-weight: 500;
+      color: #FFFFFF;
+      line-height: 28rpx;
+    }
+  }
 }
 </style>
